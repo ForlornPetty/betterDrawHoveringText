616b90f82b28c64a5294ef9726452bf6adf0ace3
Petty <90536526+ForlornPetty@users.noreply.github.com>
2023-04-15 12:16:15 +0800

    Add files via upload

--- src/main/java/com/petty/betterdrawhoveringtext/betterDrawHoveringText.java
+++ src/main/java/com/petty/betterdrawhoveringtext/betterDrawHoveringText.java
@@ -1,11 +1,8 @@
 package com.petty.betterdrawhoveringtext;
 
 import net.minecraft.client.gui.FontRenderer;
-import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.RenderHelper;
-import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.client.event.RenderTooltipEvent;
@@ -15,23 +12,37 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.lwjgl.input.Mouse;
-import org.lwjgl.opengl.GL11;
 
 import javax.annotation.Nonnull;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static net.minecraftforge.fml.client.config.GuiUtils.drawGradientRect;
 
 @Mod(modid = "bdht", name = "betterDrawHoveringText", version = "1.0.0")
 public class betterDrawHoveringText {
-    static double subtractY;static double inertia;static ItemStack itemStack;
+    static double inertia;
+    static ItemStack itemStack;
+    Map<ItemStack, Double> itemStackDoubleMap = new HashMap<>();
+
     @Mod.EventHandler
-    void preInit(FMLPreInitializationEvent event) {MinecraftForge.EVENT_BUS.register(this);}
+    void preInit(FMLPreInitializationEvent event) {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
     @SubscribeEvent(priority = EventPriority.LOWEST)
-    void RenderTooltipEvent(RenderTooltipEvent.Pre renderTooltipEvent) {drawHoveringText(renderTooltipEvent.getStack(), renderTooltipEvent.getLines(), renderTooltipEvent.getX(), renderTooltipEvent.getY(), renderTooltipEvent.getScreenWidth(), renderTooltipEvent.getScreenHeight(), renderTooltipEvent.getMaxWidth(), renderTooltipEvent.getFontRenderer());renderTooltipEvent.setCanceled(true);}
+    void RenderTooltipEvent(RenderTooltipEvent.Pre renderTooltipEvent) {
+        drawHoveringText(renderTooltipEvent.getStack(), renderTooltipEvent.getLines(), renderTooltipEvent.getX(), renderTooltipEvent.getY(), renderTooltipEvent.getScreenWidth(), renderTooltipEvent.getScreenHeight(), renderTooltipEvent.getMaxWidth(), renderTooltipEvent.getFontRenderer());
+        renderTooltipEvent.setCanceled(true);
+    }
+
     void drawHoveringText(@Nonnull final ItemStack stack, List<String> textLines, int mouseX, int mouseY, int screenWidth, int screenHeight, int maxTextWidth, FontRenderer font) {
         if (!textLines.isEmpty()) {
+            if (!itemStackDoubleMap.containsKey(stack)) {
+                itemStackDoubleMap.put(stack, (double) 0);
+            }
             GlStateManager.disableRescaleNormal();
             RenderHelper.disableStandardItemLighting();
             GlStateManager.disableLighting();
@@ -99,6 +110,16 @@ public class betterDrawHoveringText {
             } else if (tooltipY + tooltipHeight + 4 > screenHeight) {
                 tooltipY = screenHeight - tooltipHeight - 4;
             }
+            if (itemStack != stack) {
+                inertia = 0;
+                itemStack = stack;
+            } else if (tooltipHeight <= screenHeight) {
+                itemStackDoubleMap.replace(stack, (double) 0);
+            } else {
+                inertia -= inertia / 20 - MathHelper.clamp(Mouse.getDWheel(), -4, 4);
+                itemStackDoubleMap.replace(stack, MathHelper.clamp(itemStackDoubleMap.get(stack) + inertia, 0, tooltipHeight + (double) font.FONT_HEIGHT / 2 + 4 - screenHeight));
+            }
+            tooltipY += itemStackDoubleMap.get(stack);
             final int zLevel = 300;
             int backgroundColor = 0xF0100010;
             int borderColorStart = 0x505000FF;
@@ -118,20 +139,10 @@ public class betterDrawHoveringText {
             drawGradientRect(zLevel, tooltipX - 3, tooltipY - 3, tooltipX + tooltipTextWidth + 3, tooltipY - 3 + 1, borderColorStart, borderColorStart);
             drawGradientRect(zLevel, tooltipX - 3, tooltipY + tooltipHeight + 2, tooltipX + tooltipTextWidth + 3, tooltipY + tooltipHeight + 3, borderColorEnd, borderColorEnd);
             MinecraftForge.EVENT_BUS.post(new RenderTooltipEvent.PostBackground(stack, textLines, tooltipX, tooltipY, font, tooltipTextWidth, tooltipHeight));
-            if (itemStack != stack) {
-                inertia = 0;
-                subtractY = 0;
-                itemStack = stack;
-            } else if (tooltipHeight <= screenHeight) {
-                subtractY = 0;
-            } else {
-                inertia -= inertia / 20 - MathHelper.clamp(Mouse.getDWheel(), -2, 2);
-                subtractY = MathHelper.clamp(subtractY + inertia, 0, tooltipHeight + (double) font.FONT_HEIGHT / 2- screenHeight);
-            }
             int tooltipTop = tooltipY;
             for (int lineNumber = 0; lineNumber < textLines.size(); ++lineNumber) {
                 String line = textLines.get(lineNumber);
-                font.drawStringWithShadow(line, (float) tooltipX, (float) (tooltipY + subtractY), -1);
+                font.drawStringWithShadow(line, (float) tooltipX, (float) (tooltipY), -1);
                 if (lineNumber + 1 == titleLinesCount) {
                     tooltipY += 2;
                 }
